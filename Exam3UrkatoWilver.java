82b73953833459b1caac14993c7fa55c9724e581
urkatowilver <108776609+urkatowilver@users.noreply.github.com>
2022-12-10 19:32:52 -0600

    first commit

---
+++ Exam3UrkatoWilver.java
@@ -0,0 +1,13 @@
+
+public class Exam3UrkatoWilver {
+	public static void main(String[] args ) {
+	int[][] wilt= {{12,13,34,12}, {23,24,34,25},} ;
+	
+	//System.out.println(wilt[0][2]);
+	for(int i=0; i<wilt.length; i++) {
+		for(int j=0; j<wilt[i].length; j++) {
+			System.out.print(wilt[i][j]);
+		}
+	}
+}
+}
